c81dfc975186d7de893dfb741c7fc8c3d78c7f27
marcela <marcela.a.pereira@gmail.com>
2020-10-21 12:07:53 -0300

    teste

---
+++ src/veins/modules/application/out/gcc-debug/src/veins/modules/application/icarnew/Channel.d
@@ -0,0 +1,62 @@
+../out/gcc-debug/src/veins/modules/application/icarnew/Channel.o: \
+ veins/modules/application/icarnew/Channel.cc \
+ veins/modules/application/icarnew/Channel.h \
+ veins/modules/application/icarnew/./messages/ICMessage_m.h \
+ /home/marcela/Simulacao/omnetpp-5.1.1/include/omnetpp.h \
+ veins/base/utils/Coord.h veins/base/utils/MiXiMDefs.h \
+ veins/base/utils/miximkerneldefs.h veins/base/utils/FWMath.h \
+ veins/modules/messages/WaveShortMessage_m.h \
+ veins/modules/application/icarnew/CommState.h \
+ veins/modules/application/icarnew/Agent.h \
+ veins/modules/application/icarnew/MobilityInfo.h veins/base/utils/Move.h \
+ veins/modules/application/icarnew/CommPerformance.h \
+ veins/modules/application/icarnew/CommInfo.h \
+ veins/modules/application/icarnew/Route.h \
+ veins/modules/application/icarnew/ChannelMobility.h \
+ veins/modules/application/icarnew/QoCChannel.h \
+ veins/modules/application/icarnew/QoCInfo.h \
+ veins/modules/application/icarnew/Timeliness.h \
+ veins/modules/application/icarnew/TimelinessChannel.h \
+ veins/modules/application/icarnew/RadiusEstimatorChannel.h
+
+veins/modules/application/icarnew/Channel.h:
+
+veins/modules/application/icarnew/./messages/ICMessage_m.h:
+
+/home/marcela/Simulacao/omnetpp-5.1.1/include/omnetpp.h:
+
+veins/base/utils/Coord.h:
+
+veins/base/utils/MiXiMDefs.h:
+
+veins/base/utils/miximkerneldefs.h:
+
+veins/base/utils/FWMath.h:
+
+veins/modules/messages/WaveShortMessage_m.h:
+
+veins/modules/application/icarnew/CommState.h:
+
+veins/modules/application/icarnew/Agent.h:
+
+veins/modules/application/icarnew/MobilityInfo.h:
+
+veins/base/utils/Move.h:
+
+veins/modules/application/icarnew/CommPerformance.h:
+
+veins/modules/application/icarnew/CommInfo.h:
+
+veins/modules/application/icarnew/Route.h:
+
+veins/modules/application/icarnew/ChannelMobility.h:
+
+veins/modules/application/icarnew/QoCChannel.h:
+
+veins/modules/application/icarnew/QoCInfo.h:
+
+veins/modules/application/icarnew/Timeliness.h:
+
+veins/modules/application/icarnew/TimelinessChannel.h:
+
+veins/modules/application/icarnew/RadiusEstimatorChannel.h:
